2264e7de67055a9dcee14aab8d67f7254ed9c432
77joses <njigely98@gmail.com>
2026-05-16 04:29:04 +0300

    Create pathways.js

---
+++ src/data/pathways.js
@@ -0,0 +1,24 @@
+const pathways = [
+  {
+    id: 1,
+    name: "STEM",
+    description:
+      "Science, Technology, Engineering and Mathematics pathways.",
+  },
+
+  {
+    id: 2,
+    name: "Social Sciences",
+    description:
+      "Business, humanities and communication pathways.",
+  },
+
+  {
+    id: 3,
+    name: "Arts & Sports Science",
+    description:
+      "Creative arts, performing arts and sports pathways.",
+  },
+];
+
+export default pathways;
