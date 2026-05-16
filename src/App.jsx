66fbdc3e4e1962296c4fd7845300792fc1ec2847
77joses <njigely98@gmail.com>
2026-05-16 04:24:07 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,29 @@
 export default function App() {
   return (
-    <div
-      style={{
-        minHeight: "100vh",
-        background: "#07152b",
-        color: "white",
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "center",
-        alignItems: "center",
-        fontFamily: "Arial",
-        textAlign: "center"
-      }}
-    >
-      <h1 style={{ fontSize: "4rem", marginBottom: "10px" }}>
-        SwiftPath
-      </h1>
+    <div className="container">
 
-      <p style={{ fontSize: "1.4rem", opacity: 0.8 }}>
-        Pathways to Success
-      </p>
+      <section className="hero">
+
+        <h1>SwiftPath</h1>
+
+        <p>
+          Discover the pathway built for your future.
+          SwiftPath helps CBC learners explore pathways,
+          subjects, schools, and careers through intelligent guidance.
+        </p>
+
+        <div className="buttons">
+          <button className="primary">
+            Start Analysis
+          </button>
+
+          <button className="secondary">
+            Explore Pathways
+          </button>
+        </div>
+
+      </section>
+
     </div>
   );
 }
