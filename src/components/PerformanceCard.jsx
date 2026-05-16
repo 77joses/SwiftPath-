e5a9ebe79cc31a6cba2a585cf95b62a6083f9052
77joses <njigely98@gmail.com>
2026-05-16 05:09:02 +0300

    Create PerformanceCard.jsx

---
+++ src/components/PerformanceCard.jsx
@@ -0,0 +1,37 @@
+import performanceBands from "../data/performanceBands";
+
+export default function PerformanceCard({ subject }) {
+  return (
+    <div
+      style={{
+        background: "#10213d",
+        padding: "20px",
+        borderRadius: "16px",
+        marginTop: "16px",
+        width: "100%",
+        maxWidth: "700px",
+      }}
+    >
+      <h3>{subject}</h3>
+
+      <select
+        style={{
+          marginTop: "15px",
+          width: "100%",
+          padding: "14px",
+          borderRadius: "12px",
+          border: "none",
+          fontSize: "1rem",
+        }}
+      >
+        <option>Select performance band</option>
+
+        {performanceBands.map((band, index) => (
+          <option key={index}>
+            {band}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
+}
