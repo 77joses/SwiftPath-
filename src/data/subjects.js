77b94112174f7bd98ea3aed3c4805ff66e49654b
77joses <njigely98@gmail.com>
2026-05-16 05:05:45 +0300

    Create subjects.js

---
+++ src/data/subjects.js
@@ -0,0 +1,17 @@
+const subjects = [
+  "Mathematics",
+  "English",
+  "Kiswahili",
+  "Integrated Science",
+  "Social Studies",
+  "CRE",
+  "Agriculture & Nutrition",
+  "Pre-Technical Studies",
+  "Business Studies",
+  "Computer Science",
+  "Visual Arts",
+  "Performing Arts",
+  "Home Science"
+];
+
+export default subjects;
