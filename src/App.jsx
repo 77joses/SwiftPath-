5a43b4079ae6f9f5432146664d07dfa07d3dd942
77joses <njigely98@gmail.com>
2026-05-16 04:05:29 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,1 @@
-export default function App() {
-  return (
-    <div
-      style={{
-        minHeight: "100vh",
-        background: "#07152b",
-        color: "white",
-        padding: "40px",
-        fontFamily: "Arial"
-      }}
-    >
-      <h1>SwiftPath</h1>
-      <p>Pathways to Success</p>
-    </div>
-  );
-}
+
