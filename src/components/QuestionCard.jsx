6433146301d995f66457329e07d2da0942aecb0c
77joses <njigely98@gmail.com>
2026-05-16 04:53:08 +0300

    Update QuestionCard.jsx

--- src/components/QuestionCard.jsx
+++ src/components/QuestionCard.jsx
@@ -1,4 +1,8 @@
-export default function QuestionCard({ question }) {
+export default function QuestionCard({
+  question,
+  type,
+  options
+}) {
   return (
     <div
       style={{
@@ -12,17 +16,39 @@ export default function QuestionCard({ question }) {
     >
       <h3>{question}</h3>
 
-      <div
-        style={{
-          marginTop: "20px",
-          display: "flex",
-          gap: "12px",
-          flexWrap: "wrap",
-        }}
-      >
-        <button className="primary">Yes</button>
-        <button className="secondary">No</button>
-      </div>
+      {type === "yesno" && (
+        <div
+          style={{
+            marginTop: "20px",
+            display: "flex",
+            gap: "12px",
+          }}
+        >
+          <button className="primary">Yes</button>
+          <button className="secondary">No</button>
+        </div>
+      )}
+
+      {type === "select" && (
+        <select
+          style={{
+            marginTop: "20px",
+            width: "100%",
+            padding: "14px",
+            borderRadius: "12px",
+            border: "none",
+            fontSize: "1rem",
+          }}
+        >
+          <option>Select option</option>
+
+          {options.map((option, index) => (
+            <option key={index}>
+              {option}
+            </option>
+          ))}
+        </select>
+      )}
     </div>
   );
 }
