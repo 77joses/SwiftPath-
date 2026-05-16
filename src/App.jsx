c9ef15d72e3c41d5f5b2963defe3419cce3918f5
77joses <njigely98@gmail.com>
2026-05-16 04:33:05 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,6 @@
+import pathways from "./data/pathways";
+import PathwayCard from "./components/PathwayCard";
+
 export default function App() {
   return (
     <div className="container">
@@ -20,6 +23,24 @@ export default function App() {
           <button className="secondary">
             Explore Pathways
           </button>
+        </div>
+
+        <div
+          style={{
+            marginTop: "60px",
+            width: "100%",
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          {pathways.map((pathway) => (
+            <PathwayCard
+              key={pathway.id}
+              name={pathway.name}
+              description={pathway.description}
+            />
+          ))}
         </div>
 
       </section>
