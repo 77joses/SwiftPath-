b9d634ef0b89a7b69d45abda88185ab323c37dcc
77joses <njigely98@gmail.com>
2026-05-16 11:33:19 +0300

    Create subjectCombinations.js

---
+++ src/data/subjectCombinations.js
@@ -0,0 +1,11 @@
+const subjectCombinations = [
+  "Physics, Chemistry, Advanced Mathematics",
+  "Biology, Chemistry, Agriculture",
+  "Computer Science, Mathematics, Physics",
+  "Business Studies, Economics, Mathematics",
+  "Literature, History, CRE",
+  "Fine Arts, Music, Theatre Arts",
+  "Sports Science, Biology, Nutrition"
+];
+
+export default subjectCombinations;
