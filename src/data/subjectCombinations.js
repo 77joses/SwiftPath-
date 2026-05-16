6891100562767ae9eb74fa7f346180617d20c3d2
77joses <njigely98@gmail.com>
2026-05-16 11:56:34 +0300

    Update subjectCombinations.js

--- src/data/subjectCombinations.js
+++ src/data/subjectCombinations.js
@@ -1,43 +1,16 @@
 const subjectCombinations = [
-  {
-    pathway: "STEM",
-    track: "Pure Sciences",
-    combinations: [
-      "Advanced Mathematics, Biology, Chemistry",
-      "Advanced Mathematics, Biology, Physics",
-      "Advanced Mathematics, Chemistry, Physics",
-      "Biology, Chemistry, Physics"
-    ]
-  },
-
-  {
-    pathway: "STEM",
-    track: "Applied Sciences",
-    combinations: [
-      "Agriculture, Computer Studies",
-      "Agriculture, Home Science",
-      "Computer Studies, Geography"
-    ]
-  },
-
-  {
-    pathway: "Social Sciences",
-    track: "Humanities & Business Studies",
-    combinations: [
-      "Business Studies, History & Citizenship",
-      "CRE, Geography",
-      "History & Citizenship, Geography"
-    ]
-  },
-
-  {
-    pathway: "Arts & Sports Science",
-    track: "Arts",
-    combinations: [
-      "Fine Arts, Theatre & Film",
-      "Music & Dance, Theatre & Film"
-    ]
-  }
+  "Advanced Mathematics, Biology, Chemistry",
+  "Advanced Mathematics, Biology, Physics",
+  "Advanced Mathematics, Chemistry, Physics",
+  "Biology, Chemistry, Physics",
+  "Agriculture, Computer Studies",
+  "Agriculture, Home Science",
+  "Computer Studies, Geography",
+  "Business Studies, History & Citizenship",
+  "CRE, Geography",
+  "History & Citizenship, Geography",
+  "Fine Arts, Theatre & Film",
+  "Music & Dance, Theatre & Film"
 ];
 
 export default subjectCombinations;
