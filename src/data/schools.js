fc94619707986c5c45fc8f1302bd042c5b822b27
77joses <njigely98@gmail.com>
2026-05-16 18:56:15 +0300

    Create schools.js

---
+++ src/data/schools.js
@@ -0,0 +1,39 @@
+const schools = [
+  {
+    name: "Alliance High School",
+    county: "Kiambu",
+    pathway: "STEM"
+  },
+
+  {
+    name: "Mang'u High School",
+    county: "Kiambu",
+    pathway: "STEM"
+  },
+
+  {
+    name: "Kenyatta High School Mahiga",
+    county: "Nyeri",
+    pathway: "Social Sciences"
+  },
+
+  {
+    name: "Moi Girls Nairobi",
+    county: "Nairobi",
+    pathway: "Arts & Sports Science"
+  },
+
+  {
+    name: "Lenana School",
+    county: "Nairobi",
+    pathway: "STEM"
+  },
+
+  {
+    name: "Maseno School",
+    county: "Kisumu",
+    pathway: "STEM"
+  }
+];
+
+export default schools;
