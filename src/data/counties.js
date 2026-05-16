049810d2283eb60072b2dfc6fcd5b28f2e227ab4
77joses <njigely98@gmail.com>
2026-05-16 18:24:44 +0300

    Create counties.js

---
+++ src/data/counties.js
@@ -0,0 +1,14 @@
+const counties = [
+  "Nairobi",
+  "Kiambu",
+  "Nakuru",
+  "Mombasa",
+  "Kisumu",
+  "Nyeri",
+  "Meru",
+  "Machakos",
+  "Uasin Gishu",
+  "Kakamega"
+];
+
+export default counties;
