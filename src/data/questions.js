f07939361eb221947b1fe2d078c30afbc25ed628
77joses <njigely98@gmail.com>
2026-05-16 04:37:53 +0300

    Create questions.js

---
+++ src/data/questions.js
@@ -0,0 +1,28 @@
+const questions = [
+  {
+    id: 1,
+    question: "Which subjects do you enjoy most?",
+  },
+
+  {
+    id: 2,
+    question: "Do you enjoy solving complex problems?",
+  },
+
+  {
+    id: 3,
+    question: "Do you enjoy creative activities?",
+  },
+
+  {
+    id: 4,
+    question: "Do you prefer practical or theoretical learning?",
+  },
+
+  {
+    id: 5,
+    question: "Which careers interest you most?",
+  },
+];
+
+export default questions;
