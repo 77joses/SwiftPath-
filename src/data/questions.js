7cab2d7e9dcc04bab4ae99d4b6d47b728ce1a13d
77joses <njigely98@gmail.com>
2026-05-16 04:51:29 +0300

    Update questions.js

--- src/data/questions.js
+++ src/data/questions.js
@@ -1,28 +1,73 @@
 const questions = [
   {
     id: 1,
+    type: "select",
     question: "Which subjects do you enjoy most?",
+    options: [
+      "Mathematics",
+      "English",
+      "Kiswahili",
+      "Integrated Science",
+      "Social Studies",
+      "CRE",
+      "Agriculture & Nutrition",
+      "Pre-Technical Studies",
+      "Business Studies",
+      "Computer Science",
+      "Visual Arts",
+      "Performing Arts",
+      "Home Science",
+      "French",
+      "German",
+      "Arabic"
+    ]
   },
 
   {
     id: 2,
-    question: "Do you enjoy solving complex problems?",
+    type: "yesno",
+    question: "Do you enjoy solving complex problems?"
   },
 
   {
     id: 3,
-    question: "Do you enjoy creative activities?",
+    type: "yesno",
+    question: "Do you enjoy creative activities?"
   },
 
   {
     id: 4,
+    type: "select",
     question: "Do you prefer practical or theoretical learning?",
+    options: [
+      "Practical",
+      "Theoretical",
+      "Both"
+    ]
   },
 
   {
     id: 5,
+    type: "select",
     question: "Which careers interest you most?",
-  },
+    options: [
+      "Doctor",
+      "Engineer",
+      "Lawyer",
+      "Teacher",
+      "Software Developer",
+      "Pilot",
+      "Architect",
+      "Journalist",
+      "Graphic Designer",
+      "Musician",
+      "Athlete",
+      "Entrepreneur",
+      "Scientist",
+      "Accountant",
+      "Nurse"
+    ]
+  }
 ];
 
 export default questions;
