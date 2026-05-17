29da819572daf4305daaddf0f0aa4bb5d16b1ecd
77joses <njigely98@gmail.com>
2026-05-17 14:06:25 +0300

    Create othayaSchools.js

---
+++ src/data/othayaSchools.js
@@ -0,0 +1,289 @@
+const othayaSchools = [
+
+  // C2 EXTRA COUNTY
+
+  {
+    name: "Othaya Boys High School",
+
+    county: "Nyeri",
+
+    subcounty: "Othaya",
+
+    category: "C2",
+
+    gender: "Boys",
+
+    accommodation: "Boarding",
+
+    pathways: [
+      "STEM",
+      "Social Sciences",
+      "Arts & Sports Science"
+    ],
+
+    tracks: [
+      "Pure Sciences",
+      "Applied Sciences",
+      "Technical Studies",
+      "Humanities & Business Studies",
+      "Languages & Literature",
+      "Arts",
+      "Sports & Recreation"
+    ]
+  },
+
+  {
+    name: "Nyakinyua Girls High School",
+
+    county: "Nyeri",
+
+    subcounty: "Othaya",
+
+    category: "C2",
+
+    gender: "Girls",
+
+    accommodation: "Boarding",
+
+    pathways: [
+      "STEM",
+      "Social Sciences",
+      "Arts & Sports Science"
+    ],
+
+    tracks: [
+      "Pure Sciences",
+      "Applied Sciences",
+      "Technical Studies",
+      "Humanities & Business Studies",
+      "Languages & Literature",
+      "Arts",
+      "Sports & Recreation"
+    ]
+  },
+
+  // C3 COUNTY
+
+  {
+    name: "Chinga Boys High School",
+
+    county: "Nyeri",
+
+    subcounty: "Othaya",
+
+    category: "C3",
+
+    gender: "Boys",
+
+    accommodation: "Boarding",
+
+    pathways: [
+      "STEM",
+      "Social Sciences"
+    ],
+
+    tracks: [
+      "Pure Sciences",
+      "Applied Sciences",
+      "Technical Studies",
+      "Humanities & Business Studies",
+      "Languages & Literature"
+    ]
+  },
+
+  {
+    name: "Mahiga Girls Secondary School",
+
+    county: "Nyeri",
+
+    subcounty: "Othaya",
+
+    category: "C3",
+
+    gender: "Girls",
+
+    accommodation: "Boarding",
+
+    pathways: [
+      "STEM",
+      "Social Sciences"
+    ],
+
+    tracks: [
+      "Pure Sciences",
+      "Applied Sciences",
+      "Technical Studies",
+      "Humanities & Business Studies",
+      "Languages & Literature"
+    ]
+  },
+
+  {
+    name: "Ichamara Secondary School",
+
+    county: "Nyeri",
+
+    subcounty: "Othaya",
+
+    category: "C3",
+
+    gender: "Mixed",
+
+    accommodation: "Day",
+
+    pathways: [
+      "STEM",
+      "Social Sciences"
+    ],
+
+    tracks: [
+      "Pure Sciences",
+      "Applied Sciences",
+      "Humanities & Business Studies"
+    ]
+  },
+
+  // C4 SUBCOUNTY
+
+  {
+    name: "Gatugi Secondary School",
+
+    county: "Nyeri",
+
+    subcounty: "Othaya",
+
+    category: "C4",
+
+    gender: "Mixed",
+
+    accommodation: "Day",
+
+    pathways: [
+      "STEM"
+    ],
+
+    tracks: [
+      "Pure Sciences",
+      "Applied Sciences",
+      "Technical Studies"
+    ]
+  },
+
+  {
+    name: "Gikira Secondary School",
+
+    county: "Nyeri",
+
+    subcounty: "Othaya",
+
+    category: "C4",
+
+    gender: "Mixed",
+
+    accommodation: "Day",
+
+    pathways: [
+      "STEM"
+    ],
+
+    tracks: [
+      "Applied Sciences",
+      "Technical Studies"
+    ]
+  },
+
+  {
+    name: "Karima Girls Secondary School",
+
+    county: "Nyeri",
+
+    subcounty: "Othaya",
+
+    category: "C4",
+
+    gender: "Girls",
+
+    accommodation: "Day",
+
+    pathways: [
+      "STEM"
+    ],
+
+    tracks: [
+      "Applied Sciences",
+      "Technical Studies"
+    ]
+  },
+
+  {
+    name: "Muthuaini Secondary School",
+
+    county: "Nyeri",
+
+    subcounty: "Othaya",
+
+    category: "C4",
+
+    gender: "Mixed",
+
+    accommodation: "Day",
+
+    pathways: [
+      "STEM"
+    ],
+
+    tracks: [
+      "Applied Sciences",
+      "Technical Studies"
+    ]
+  },
+
+  {
+    name: "Rware Secondary School",
+
+    county: "Nyeri",
+
+    subcounty: "Othaya",
+
+    category: "C4",
+
+    gender: "Mixed",
+
+    accommodation: "Day",
+
+    pathways: [
+      "STEM"
+    ],
+
+    tracks: [
+      "Applied Sciences",
+      "Technical Studies"
+    ]
+  },
+
+  {
+    name: "Wamagana Secondary School",
+
+    county: "Nyeri",
+
+    subcounty: "Othaya",
+
+    category: "C4",
+
+    gender: "Mixed",
+
+    accommodation: "Day",
+
+    pathways: [
+      "STEM"
+    ],
+
+    tracks: [
+      "Applied Sciences",
+      "Technical Studies"
+    ]
+  }
+
+];
+
+export default othayaSchools;
