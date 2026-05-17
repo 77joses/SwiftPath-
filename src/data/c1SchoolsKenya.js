34147881fbfb599b679a07dd55cf53f4dbff374f
77joses <njigely98@gmail.com>
2026-05-17 20:31:16 +0300

    Create c1SchoolsKenya.js

---
+++ src/data/c1SchoolsKenya.js
@@ -0,0 +1,240 @@
+const c1SchoolsKenya = [
+
+  // NAIROBI
+
+  {
+    name: "Alliance High School",
+    county: "Kiambu",
+    category: "C1",
+    gender: "Boys",
+    accommodation: "Boarding",
+    pathways: [
+      "STEM",
+      "Social Sciences",
+      "Arts & Sports Science"
+    ]
+  },
+
+  {
+    name: "Alliance Girls High School",
+    county: "Kiambu",
+    category: "C1",
+    gender: "Girls",
+    accommodation: "Boarding",
+    pathways: [
+      "STEM",
+      "Social Sciences",
+      "Arts & Sports Science"
+    ]
+  },
+
+  {
+    name: "Kenya High School",
+    county: "Nairobi",
+    category: "C1",
+    gender: "Girls",
+    accommodation: "Boarding",
+    pathways: [
+      "STEM",
+      "Social Sciences",
+      "Arts & Sports Science"
+    ]
+  },
+
+  {
+    name: "Starehe Boys Centre",
+    county: "Nairobi",
+    category: "C1",
+    gender: "Boys",
+    accommodation: "Boarding",
+    pathways: [
+      "STEM",
+      "Social Sciences",
+      "Arts & Sports Science"
+    ]
+  },
+
+  // CENTRAL
+
+  {
+    name: "Nyeri High School",
+    county: "Nyeri",
+    category: "C1",
+    gender: "Boys",
+    accommodation: "Boarding",
+    pathways: [
+      "STEM",
+      "Social Sciences",
+      "Arts & Sports Science"
+    ]
+  },
+
+  {
+    name: "Kagumo High School",
+    county: "Nyeri",
+    category: "C1",
+    gender: "Boys",
+    accommodation: "Boarding",
+    pathways: [
+      "STEM",
+      "Social Sciences",
+      "Arts & Sports Science"
+    ]
+  },
+
+  {
+    name: "Mang'u High School",
+    county: "Kiambu",
+    category: "C1",
+    gender: "Boys",
+    accommodation: "Boarding",
+    pathways: [
+      "STEM",
+      "Social Sciences",
+      "Arts & Sports Science"
+    ]
+  },
+
+  // RIFT VALLEY
+
+  {
+    name: "Kapsabet Boys High School",
+    county: "Nandi",
+    category: "C1",
+    gender: "Boys",
+    accommodation: "Boarding",
+    pathways: [
+      "STEM",
+      "Social Sciences",
+      "Arts & Sports Science"
+    ]
+  },
+
+  {
+    name: "Moi Girls High School Eldoret",
+    county: "Uasin Gishu",
+    category: "C1",
+    gender: "Girls",
+    accommodation: "Boarding",
+    pathways: [
+      "STEM",
+      "Social Sciences",
+      "Arts & Sports Science"
+    ]
+  },
+
+  // EASTERN
+
+  {
+    name: "Meru School",
+    county: "Meru",
+    category: "C1",
+    gender: "Boys",
+    accommodation: "Boarding",
+    pathways: [
+      "STEM",
+      "Social Sciences",
+      "Arts & Sports Science"
+    ]
+  },
+
+  {
+    name: "Mbooni Girls High School",
+    county: "Makueni",
+    category: "C1",
+    gender: "Girls",
+    accommodation: "Boarding",
+    pathways: [
+      "STEM",
+      "Social Sciences",
+      "Arts & Sports Science"
+    ]
+  },
+
+  // WESTERN
+
+  {
+    name: "Kakamega High School",
+    county: "Kakamega",
+    category: "C1",
+    gender: "Boys",
+    accommodation: "Boarding",
+    pathways: [
+      "STEM",
+      "Social Sciences",
+      "Arts & Sports Science"
+    ]
+  },
+
+  {
+    name: "Butere Girls High School",
+    county: "Kakamega",
+    category: "C1",
+    gender: "Girls",
+    accommodation: "Boarding",
+    pathways: [
+      "STEM",
+      "Social Sciences",
+      "Arts & Sports Science"
+    ]
+  },
+
+  // NYANZA
+
+  {
+    name: "Maseno School",
+    county: "Kisumu",
+    category: "C1",
+    gender: "Boys",
+    accommodation: "Boarding",
+    pathways: [
+      "STEM",
+      "Social Sciences",
+      "Arts & Sports Science"
+    ]
+  },
+
+  {
+    name: "Kisumu Girls High School",
+    county: "Kisumu",
+    category: "C1",
+    gender: "Girls",
+    accommodation: "Boarding",
+    pathways: [
+      "STEM",
+      "Social Sciences",
+      "Arts & Sports Science"
+    ]
+  },
+
+  // COAST
+
+  {
+    name: "Shimo La Tewa High School",
+    county: "Mombasa",
+    category: "C1",
+    gender: "Boys",
+    accommodation: "Boarding",
+    pathways: [
+      "STEM",
+      "Social Sciences",
+      "Arts & Sports Science"
+    ]
+  },
+
+  {
+    name: "Mama Ngina Girls High School",
+    county: "Mombasa",
+    category: "C1",
+    gender: "Girls",
+    accommodation: "Boarding",
+    pathways: [
+      "STEM",
+      "Social Sciences",
+      "Arts & Sports Science"
+    ]
+  }
+
+];
+
+export default c1SchoolsKenya;
