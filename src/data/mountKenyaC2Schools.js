569c2d353300051eb9548f21357dbf9085cea070
77joses <njigely98@gmail.com>
2026-05-17 20:47:18 +0300

    Create mountKenyaC2Schools.js

---
+++ src/data/mountKenyaC2Schools.js
@@ -0,0 +1,165 @@
+const mountKenyaC2Schools = [
+
+  // NYERI
+
+  {
+    name: "Nyeri Baptist High School",
+    county: "Nyeri",
+    category: "C2",
+    gender: "Girls",
+    accommodation: "Boarding"
+  },
+
+  {
+    name: "Kangubiri Girls High School",
+    county: "Nyeri",
+    category: "C2",
+    gender: "Girls",
+    accommodation: "Boarding"
+  },
+
+  {
+    name: "Dedan Kimathi Memorial High School",
+    county: "Nyeri",
+    category: "C2",
+    gender: "Boys",
+    accommodation: "Boarding"
+  },
+
+  {
+    name: "Othaya Boys High School",
+    county: "Nyeri",
+    category: "C2",
+    gender: "Boys",
+    accommodation: "Boarding"
+  },
+
+  // KIAMBU
+
+  {
+    name: "Loreto High School Limuru",
+    county: "Kiambu",
+    category: "C2",
+    gender: "Girls",
+    accommodation: "Boarding"
+  },
+
+  {
+    name: "Kiambu High School",
+    county: "Kiambu",
+    category: "C2",
+    gender: "Boys",
+    accommodation: "Boarding"
+  },
+
+  {
+    name: "Thika High School",
+    county: "Kiambu",
+    category: "C2",
+    gender: "Boys",
+    accommodation: "Boarding"
+  },
+
+  // MURANG'A
+
+  {
+    name: "Murang'a High School",
+    county: "Murang'a",
+    category: "C2",
+    gender: "Boys",
+    accommodation: "Boarding"
+  },
+
+  {
+    name: "Kahuhia Girls High School",
+    county: "Murang'a",
+    category: "C2",
+    gender: "Girls",
+    accommodation: "Boarding"
+  },
+
+  // KIRINYAGA
+
+  {
+    name: "Kerugoya Boys High School",
+    county: "Kirinyaga",
+    category: "C2",
+    gender: "Boys",
+    accommodation: "Boarding"
+  },
+
+  {
+    name: "Kangaru Girls High School",
+    county: "Kirinyaga",
+    category: "C2",
+    gender: "Girls",
+    accommodation: "Boarding"
+  },
+
+  // EMBU
+
+  {
+    name: "Embu High School",
+    county: "Embu",
+    category: "C2",
+    gender: "Boys",
+    accommodation: "Boarding"
+  },
+
+  {
+    name: "Kyeni Girls High School",
+    county: "Embu",
+    category: "C2",
+    gender: "Girls",
+    accommodation: "Boarding"
+  },
+
+  // MERU
+
+  {
+    name: "Kaaga Girls High School",
+    county: "Meru",
+    category: "C2",
+    gender: "Girls",
+    accommodation: "Boarding"
+  },
+
+  {
+    name: "Nkubu High School",
+    county: "Meru",
+    category: "C2",
+    gender: "Boys",
+    accommodation: "Boarding"
+  },
+
+  // THARAKA NITHI
+
+  {
+    name: "Chogoria Girls High School",
+    county: "Tharaka Nithi",
+    category: "C2",
+    gender: "Girls",
+    accommodation: "Boarding"
+  },
+
+  // LAIKIPIA
+
+  {
+    name: "Nanyuki High School",
+    county: "Laikipia",
+    category: "C2",
+    gender: "Boys",
+    accommodation: "Boarding"
+  },
+
+  {
+    name: "Njonjo Girls High School",
+    county: "Laikipia",
+    category: "C2",
+    gender: "Girls",
+    accommodation: "Boarding"
+  }
+
+];
+
+export default mountKenyaC2Schools;
