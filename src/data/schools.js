aa8e0288c02d146ee0958bc6b8a94b47f2fc7b8a
77joses <njigely98@gmail.com>
2026-05-17 05:37:32 +0300

    Update schools.js

--- src/data/schools.js
+++ src/data/schools.js
@@ -1,39 +1,61 @@
 const schools = [
+
   {
     name: "Alliance High School",
     county: "Kiambu",
+    category: "C1",
     pathway: "STEM"
   },
 
   {
     name: "Mang'u High School",
     county: "Kiambu",
+    category: "C1",
     pathway: "STEM"
   },
 
   {
     name: "Kenyatta High School Mahiga",
     county: "Nyeri",
+    category: "C2",
     pathway: "Social Sciences"
   },
 
   {
     name: "Moi Girls Nairobi",
     county: "Nairobi",
+    category: "C1",
     pathway: "Arts & Sports Science"
   },
 
   {
     name: "Lenana School",
     county: "Nairobi",
+    category: "C1",
     pathway: "STEM"
   },
 
   {
     name: "Maseno School",
     county: "Kisumu",
+    category: "C1",
+    pathway: "STEM"
+  },
+
+  {
+    name: "Meru School",
+    county: "Meru",
+    category: "C2",
+    pathway: "STEM"
+  },
+
+  {
+    name: "Nyeri High School",
+    county: "Nyeri",
+    category: "C1",
     pathway: "STEM"
   }
+
 ];
 
 export default schools;
