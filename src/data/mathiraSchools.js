6f37ff519cbeab850ff0a47c339ef0f83fe2f3bf
77joses <njigely98@gmail.com>
2026-05-17 13:58:33 +0300

    Create mathiraSchools.js

---
+++ src/data/mathiraSchools.js
@@ -0,0 +1,389 @@
+const mathiraSchools = [
+
+  // C1 NATIONAL
+
+  {
+    name: "Kagumo High School",
+
+    county: "Nyeri",
+
+    subcounty: "Mathira",
+
+    category: "C1",
+
+    gender: "Boys",
+
+    accommodation: "Boarding",
+
+    pathways: [
+      "STEM",
+      "Social Sciences",
+      "Arts & Sports Science"
+    ],
+
+    tracks: [
+      "Pure Sciences",
+      "Applied Sciences",
+      "Technical Studies",
+      "Humanities & Business Studies",
+      "Languages & Literature",
+      "Arts",
+      "Sports & Recreation"
+    ]
+  },
+
+  // C2 EXTRA COUNTY
+
+  {
+    name: "Karatina Girls High School",
+
+    county: "Nyeri",
+
+    subcounty: "Mathira",
+
+    category: "C2",
+
+    gender: "Girls",
+
+    accommodation: "Boarding",
+
+    pathways: [
+      "STEM",
+      "Social Sciences",
+      "Arts & Sports Science"
+    ],
+
+    tracks: [
+      "Pure Sciences",
+      "Applied Sciences",
+      "Technical Studies",
+      "Humanities & Business Studies",
+      "Languages & Literature",
+      "Arts",
+      "Sports & Recreation"
+    ]
+  },
+
+  {
+    name: "Mathaithi Secondary School",
+
+    county: "Nyeri",
+
+    subcounty: "Mathira",
+
+    category: "C2",
+
+    gender: "Boys",
+
+    accommodation: "Boarding",
+
+    pathways: [
+      "STEM",
+      "Social Sciences",
+      "Arts & Sports Science"
+    ],
+
+    tracks: [
+      "Pure Sciences",
+      "Applied Sciences",
+      "Technical Studies",
+      "Humanities & Business Studies",
+      "Languages & Literature",
+      "Arts",
+      "Sports & Recreation"
+    ]
+  },
+
+  {
+    name:
+      "Mweru Girls High School",
+
+    county: "Nyeri",
+
+    subcounty: "Mathira",
+
+    category: "C2",
+
+    gender: "Girls",
+
+    accommodation: "Boarding",
+
+    pathways: [
+      "STEM",
+      "Social Sciences",
+      "Arts & Sports Science"
+    ],
+
+    tracks: [
+      "Pure Sciences",
+      "Applied Sciences",
+      "Technical Studies",
+      "Humanities & Business Studies",
+      "Languages & Literature",
+      "Arts",
+      "Sports & Recreation"
+    ]
+  },
+
+  // C3 COUNTY
+
+  {
+    name: "Bishop Gatimu Ngandu Girls",
+
+    county: "Nyeri",
+
+    subcounty: "Mathira",
+
+    category: "C3",
+
+    gender: "Girls",
+
+    accommodation: "Boarding",
+
+    pathways: [
+      "STEM",
+      "Social Sciences"
+    ],
+
+    tracks: [
+      "Pure Sciences",
+      "Applied Sciences",
+      "Technical Studies",
+      "Humanities & Business Studies",
+      "Languages & Literature"
+    ]
+  },
+
+  {
+    name: "Karatina Boys High School",
+
+    county: "Nyeri",
+
+    subcounty: "Mathira",
+
+    category: "C3",
+
+    gender: "Boys",
+
+    accommodation: "Boarding",
+
+    pathways: [
+      "STEM",
+      "Social Sciences"
+    ],
+
+    tracks: [
+      "Pure Sciences",
+      "Applied Sciences",
+      "Technical Studies",
+      "Humanities & Business Studies",
+      "Languages & Literature"
+    ]
+  },
+
+  {
+    name: "Kiandu Secondary School",
+
+    county: "Nyeri",
+
+    subcounty: "Mathira",
+
+    category: "C3",
+
+    gender: "Mixed",
+
+    accommodation: "Day",
+
+    pathways: [
+      "STEM",
+      "Social Sciences"
+    ],
+
+    tracks: [
+      "Pure Sciences",
+      "Applied Sciences",
+      "Humanities & Business Studies"
+    ]
+  },
+
+  // C4 SUBCOUNTY
+
+  {
+    name: "Chieni Secondary School",
+
+    county: "Nyeri",
+
+    subcounty: "Mathira",
+
+    category: "C4",
+
+    gender: "Mixed",
+
+    accommodation: "Day",
+
+    pathways: ["STEM"],
+
+    tracks: [
+      "Pure Sciences",
+      "Applied Sciences",
+      "Technical Studies"
+    ]
+  },
+
+  {
+    name: "Gathioro Secondary School",
+
+    county: "Nyeri",
+
+    subcounty: "Mathira",
+
+    category: "C4",
+
+    gender: "Mixed",
+
+    accommodation: "Day",
+
+    pathways: ["STEM"],
+
+    tracks: [
+      "Pure Sciences",
+      "Applied Sciences",
+      "Technical Studies"
+    ]
+  },
+
+  {
+    name: "Gitunduti Secondary School",
+
+    county: "Nyeri",
+
+    subcounty: "Mathira",
+
+    category: "C4",
+
+    gender: "Mixed",
+
+    accommodation: "Day",
+
+    pathways: ["STEM"],
+
+    tracks: [
+      "Pure Sciences",
+      "Applied Sciences",
+      "Technical Studies"
+    ]
+  },
+
+  {
+    name: "Ihithe Secondary School",
+
+    county: "Nyeri",
+
+    subcounty: "Mathira",
+
+    category: "C4",
+
+    gender: "Mixed",
+
+    accommodation: "Day",
+
+    pathways: ["STEM"],
+
+    tracks: [
+      "Pure Sciences",
+      "Applied Sciences",
+      "Technical Studies"
+    ]
+  },
+
+  {
+    name: "Kiamariga Secondary School",
+
+    county: "Nyeri",
+
+    subcounty: "Mathira",
+
+    category: "C4",
+
+    gender: "Mixed",
+
+    accommodation: "Day",
+
+    pathways: ["STEM"],
+
+    tracks: [
+      "Pure Sciences",
+      "Applied Sciences",
+      "Technical Studies"
+    ]
+  },
+
+  {
+    name: "Kiawara Secondary School",
+
+    county: "Nyeri",
+
+    subcounty: "Mathira",
+
+    category: "C4",
+
+    gender: "Mixed",
+
+    accommodation: "Day",
+
+    pathways: ["STEM"],
+
+    tracks: [
+      "Pure Sciences",
+      "Applied Sciences",
+      "Technical Studies"
+    ]
+  },
+
+  {
+    name: "Mihuti Secondary School",
+
+    county: "Nyeri",
+
+    subcounty: "Mathira",
+
+    category: "C4",
+
+    gender: "Mixed",
+
+    accommodation: "Day",
+
+    pathways: ["STEM"],
+
+    tracks: [
+      "Pure Sciences",
+      "Applied Sciences",
+      "Technical Studies"
+    ]
+  },
+
+  {
+    name: "Muthinga Secondary School",
+
+    county: "Nyeri",
+
+    subcounty: "Mathira",
+
+    category: "C4",
+
+    gender: "Mixed",
+
+    accommodation: "Day",
+
+    pathways: ["STEM"],
+
+    tracks: [
+      "Pure Sciences",
+      "Applied Sciences",
+      "Technical Studies"
+    ]
+  }
+
+];
+
+export default mathiraSchools;
