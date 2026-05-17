72f768105a03dfedcc52383e090e06450902789e
77joses <njigely98@gmail.com>
2026-05-17 14:11:13 +0300

    Create mukurweiniSchools.js

---
+++ src/data/mukurweiniSchools.js
@@ -0,0 +1,281 @@
+const mukurweiniSchools = [
+
+  // C2 EXTRA COUNTY
+
+  {
+    name: "Mukurweini Girls High School",
+
+    county: "Nyeri",
+
+    subcounty: "Mukurwe-ini",
+
+    category: "C2",
+
+    gender: "Girls",
+
+    accommodation: "Boarding",
+
+    pathways: [
+      "STEM",
+      "Social Sciences",
+      "Arts & Sports Science"
+    ],
+
+    tracks: [
+      "Pure Sciences",
+      "Applied Sciences",
+      "Technical Studies",
+      "Humanities & Business Studies",
+      "Languages & Literature",
+      "Arts",
+      "Sports & Recreation"
+    ]
+  },
+
+  // C3 COUNTY
+
+  {
+    name: "Munyange Boys High School",
+
+    county: "Nyeri",
+
+    subcounty: "Mukurwe-ini",
+
+    category: "C3",
+
+    gender: "Boys",
+
+    accommodation: "Boarding",
+
+    pathways: [
+      "STEM",
+      "Social Sciences"
+    ],
+
+    tracks: [
+      "Pure Sciences",
+      "Applied Sciences",
+      "Technical Studies",
+      "Humanities & Business Studies",
+      "Languages & Literature"
+    ]
+  },
+
+  {
+    name: "Gikondi Boys Secondary School",
+
+    county: "Nyeri",
+
+    subcounty: "Mukurwe-ini",
+
+    category: "C3",
+
+    gender: "Boys",
+
+    accommodation: "Boarding",
+
+    pathways: [
+      "STEM",
+      "Social Sciences"
+    ],
+
+    tracks: [
+      "Pure Sciences",
+      "Applied Sciences",
+      "Technical Studies",
+      "Humanities & Business Studies",
+      "Languages & Literature"
+    ]
+  },
+
+  {
+    name: "Muhito Secondary School",
+
+    county: "Nyeri",
+
+    subcounty: "Mukurwe-ini",
+
+    category: "C3",
+
+    gender: "Mixed",
+
+    accommodation: "Day",
+
+    pathways: [
+      "STEM",
+      "Social Sciences"
+    ],
+
+    tracks: [
+      "Pure Sciences",
+      "Applied Sciences",
+      "Humanities & Business Studies"
+    ]
+  },
+
+  // C4 SUBCOUNTY
+
+  {
+    name: "Gacharage Secondary School",
+
+    county: "Nyeri",
+
+    subcounty: "Mukurwe-ini",
+
+    category: "C4",
+
+    gender: "Mixed",
+
+    accommodation: "Day",
+
+    pathways: [
+      "STEM"
+    ],
+
+    tracks: [
+      "Applied Sciences",
+      "Technical Studies"
+    ]
+  },
+
+  {
+    name: "Gathuthi Secondary School",
+
+    county: "Nyeri",
+
+    subcounty: "Mukurwe-ini",
+
+    category: "C4",
+
+    gender: "Mixed",
+
+    accommodation: "Day",
+
+    pathways: [
+      "STEM"
+    ],
+
+    tracks: [
+      "Applied Sciences",
+      "Technical Studies"
+    ]
+  },
+
+  {
+    name: "Kiahungu Secondary School",
+
+    county: "Nyeri",
+
+    subcounty: "Mukurwe-ini",
+
+    category: "C4",
+
+    gender: "Mixed",
+
+    accommodation: "Day",
+
+    pathways: [
+      "STEM"
+    ],
+
+    tracks: [
+      "Applied Sciences",
+      "Technical Studies"
+    ]
+  },
+
+  {
+    name: "Kirimukuyu Secondary School",
+
+    county: "Nyeri",
+
+    subcounty: "Mukurwe-ini",
+
+    category: "C4",
+
+    gender: "Mixed",
+
+    accommodation: "Day",
+
+    pathways: [
+      "STEM"
+    ],
+
+    tracks: [
+      "Applied Sciences",
+      "Technical Studies"
+    ]
+  },
+
+  {
+    name: "Mairo-ini Secondary School",
+
+    county: "Nyeri",
+
+    subcounty: "Mukurwe-ini",
+
+    category: "C4",
+
+    gender: "Mixed",
+
+    accommodation: "Day",
+
+    pathways: [
+      "STEM"
+    ],
+
+    tracks: [
+      "Applied Sciences",
+      "Technical Studies"
+    ]
+  },
+
+  {
+    name: "Rutune Secondary School",
+
+    county: "Nyeri",
+
+    subcounty: "Mukurwe-ini",
+
+    category: "C4",
+
+    gender: "Mixed",
+
+    accommodation: "Day",
+
+    pathways: [
+      "STEM"
+    ],
+
+    tracks: [
+      "Applied Sciences",
+      "Technical Studies"
+    ]
+  },
+
+  {
+    name: "Thangathi Secondary School",
+
+    county: "Nyeri",
+
+    subcounty: "Mukurwe-ini",
+
+    category: "C4",
+
+    gender: "Mixed",
+
+    accommodation: "Day",
+
+    pathways: [
+      "STEM"
+    ],
+
+    tracks: [
+      "Applied Sciences",
+      "Technical Studies"
+    ]
+  }
+
+];
+
+export default mukurweiniSchools;
