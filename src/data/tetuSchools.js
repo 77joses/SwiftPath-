b21cf689e34af697cf8e37471d9714cc5b23fae0
77joses <njigely98@gmail.com>
2026-05-17 14:03:34 +0300

    Create tetuSchools.js

---
+++ src/data/tetuSchools.js
@@ -0,0 +1,314 @@
+const tetuSchools = [
+
+  // C2 EXTRA COUNTY
+
+  {
+    name:
+      "Dedan Kimathi Memorial High School",
+
+    county: "Nyeri",
+
+    subcounty: "Tetu",
+
+    category: "C2",
+
+    gender: "Boys",
+
+    accommodation: "Boarding",
+
+    pathways: [
+      "STEM",
+      "Social Sciences",
+      "Arts & Sports Science"
+    ],
+
+    tracks: [
+      "Pure Sciences",
+      "Applied Sciences",
+      "Technical Studies",
+      "Humanities & Business Studies",
+      "Languages & Literature",
+      "Arts",
+      "Sports & Recreation"
+    ]
+  },
+
+  {
+    name:
+      "Kangubiri Girls High School",
+
+    county: "Nyeri",
+
+    subcounty: "Tetu",
+
+    category: "C2",
+
+    gender: "Girls",
+
+    accommodation: "Boarding",
+
+    pathways: [
+      "STEM",
+      "Social Sciences",
+      "Arts & Sports Science"
+    ],
+
+    tracks: [
+      "Pure Sciences",
+      "Applied Sciences",
+      "Technical Studies",
+      "Humanities & Business Studies",
+      "Languages & Literature",
+      "Arts",
+      "Sports & Recreation"
+    ]
+  },
+
+  // C3 COUNTY
+
+  {
+    name: "Muthuini Secondary School",
+
+    county: "Nyeri",
+
+    subcounty: "Tetu",
+
+    category: "C3",
+
+    gender: "Mixed",
+
+    accommodation: "Day",
+
+    pathways: [
+      "STEM",
+      "Social Sciences"
+    ],
+
+    tracks: [
+      "Pure Sciences",
+      "Applied Sciences",
+      "Humanities & Business Studies",
+      "Languages & Literature"
+    ]
+  },
+
+  {
+    name: "Wamagana Secondary School",
+
+    county: "Nyeri",
+
+    subcounty: "Tetu",
+
+    category: "C3",
+
+    gender: "Mixed",
+
+    accommodation: "Day",
+
+    pathways: [
+      "STEM",
+      "Social Sciences"
+    ],
+
+    tracks: [
+      "Pure Sciences",
+      "Applied Sciences",
+      "Humanities & Business Studies",
+      "Languages & Literature"
+    ]
+  },
+
+  // C4 SUBCOUNTY
+
+  {
+    name: "Chinga Girls Secondary School",
+
+    county: "Nyeri",
+
+    subcounty: "Tetu",
+
+    category: "C4",
+
+    gender: "Girls",
+
+    accommodation: "Day",
+
+    pathways: [
+      "STEM"
+    ],
+
+    tracks: [
+      "Pure Sciences",
+      "Applied Sciences",
+      "Technical Studies"
+    ]
+  },
+
+  {
+    name: "Gatondo Secondary School",
+
+    county: "Nyeri",
+
+    subcounty: "Tetu",
+
+    category: "C4",
+
+    gender: "Mixed",
+
+    accommodation: "Day",
+
+    pathways: [
+      "STEM"
+    ],
+
+    tracks: [
+      "Pure Sciences",
+      "Applied Sciences",
+      "Technical Studies"
+    ]
+  },
+
+  {
+    name: "Giakanja Secondary School",
+
+    county: "Nyeri",
+
+    subcounty: "Tetu",
+
+    category: "C4",
+
+    gender: "Mixed",
+
+    accommodation: "Day",
+
+    pathways: [
+      "STEM"
+    ],
+
+    tracks: [
+      "Pure Sciences",
+      "Applied Sciences",
+      "Technical Studies"
+    ]
+  },
+
+  {
+    name: "Gikondi Secondary School",
+
+    county: "Nyeri",
+
+    subcounty: "Tetu",
+
+    category: "C4",
+
+    gender: "Mixed",
+
+    accommodation: "Day",
+
+    pathways: [
+      "STEM"
+    ],
+
+    tracks: [
+      "Pure Sciences",
+      "Applied Sciences",
+      "Technical Studies"
+    ]
+  },
+
+  {
+    name: "Iriaini Secondary School",
+
+    county: "Nyeri",
+
+    subcounty: "Tetu",
+
+    category: "C4",
+
+    gender: "Mixed",
+
+    accommodation: "Day",
+
+    pathways: [
+      "STEM"
+    ],
+
+    tracks: [
+      "Pure Sciences",
+      "Applied Sciences",
+      "Technical Studies"
+    ]
+  },
+
+  {
+    name: "Kagumo Day Secondary School",
+
+    county: "Nyeri",
+
+    subcounty: "Tetu",
+
+    category: "C4",
+
+    gender: "Mixed",
+
+    accommodation: "Day",
+
+    pathways: [
+      "STEM"
+    ],
+
+    tracks: [
+      "Applied Sciences",
+      "Technical Studies"
+    ]
+  },
+
+  {
+    name: "Kanjogu Secondary School",
+
+    county: "Nyeri",
+
+    subcounty: "Tetu",
+
+    category: "C4",
+
+    gender: "Mixed",
+
+    accommodation: "Day",
+
+    pathways: [
+      "STEM"
+    ],
+
+    tracks: [
+      "Applied Sciences",
+      "Technical Studies"
+    ]
+  },
+
+  {
+    name: "Munyange Secondary School",
+
+    county: "Nyeri",
+
+    subcounty: "Tetu",
+
+    category: "C4",
+
+    gender: "Mixed",
+
+    accommodation: "Day",
+
+    pathways: [
+      "STEM"
+    ],
+
+    tracks: [
+      "Applied Sciences",
+      "Technical Studies"
+    ]
+  }
+
+];
+
+export default tetuSchools;
