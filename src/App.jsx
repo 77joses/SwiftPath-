9e44c03f45d21f4ce540c4839238fae361c395a8
77joses <njigely98@gmail.com>
2026-05-19 16:39:45 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -348,38 +348,6 @@ if (selectedCategory === "C1") {
 )
 }
 
-if (selectedCategory === "C2") {
-
-  const safeC2Schools =
-    mountKenyaC2Schools.map(
-      (school) => ({
-
-        ...school,
-
-        pathways:
-          school.pathways || [
-            "STEM",
-            "Social Sciences",
-            "Arts & Sports Science",
-          ],
-
-        tracks:
-          school.tracks || [],
-      })
-    );
-
-  filteredSchools = [
-
-    ...filteredSchools,
-
-    ...safeC2Schools.filter(
-      (school) =>
-        school.pathways.includes(
-          recommendedPathway
-        )
-    ),
-  ];
-}
   return (
     <div className="container">
 
