5cfcc0830777fabf8a1f6a9471ac4e68ecfa75a8
77joses <njigely98@gmail.com>
2026-05-20 05:01:56 +0300

    Create nyeriSouthSchools.js

---
+++ src/data/nyeriSouthSchools.js
@@ -0,0 +1,810 @@
+const nyeriSouthSchools = [
+
+  // C2 — EXTRA COUNTY
+
+  {
+    name: "Birithia Girls",
+    subcounty: "Nyeri South",
+    county: "Nyeri",
+    category: "C2",
+    gender: "Girls",
+    accommodation: "Boarding",
+
+    pathways: [
+      "STEM",
+      "Social Sciences",
+      "Arts & Sports Science",
+    ],
+
+    tracks: [
+      "Pure Sciences",
+      "Applied Sciences",
+      "Technical Studies",
+      "Humanities & Business Studies",
+      "Languages & Literature",
+      "Arts",
+      "Sports & Recreation",
+    ],
+
+    subjectCombinations: [
+      "Advanced Mathematics, Biology, Chemistry + 1 elective",
+      "Agriculture, Computer Studies + 1 elective",
+      "Aviation, Business Studies + 1 elective",
+      "Business Studies, History & Citizenship + 1 elective",
+      "Arabic, French + 1 elective",
+      "Fine Arts, Theatre & Film + 1 elective",
+    ],
+
+    disabilitySupport: false,
+  },
+
+  {
+    name: "Chinga Boys",
+    subcounty: "Nyeri South",
+    county: "Nyeri",
+    category: "C2",
+    gender: "Boys",
+    accommodation: "Boarding",
+
+    pathways: [
+      "STEM",
+      "Social Sciences",
+      "Arts & Sports Science",
+    ],
+
+    tracks: [
+      "Pure Sciences",
+      "Applied Sciences",
+      "Technical Studies",
+      "Humanities & Business Studies",
+      "Languages & Literature",
+      "Arts",
+      "Sports & Recreation",
+    ],
+
+    subjectCombinations: [
+      "Advanced Mathematics, Biology, Chemistry + 1 elective",
+      "Agriculture, Computer Studies + 1 elective",
+      "Aviation, Business Studies + 1 elective",
+      "Business Studies, History & Citizenship + 1 elective",
+      "Arabic, French + 1 elective",
+      "Fine Arts, Theatre & Film + 1 elective",
+    ],
+
+    disabilitySupport: false,
+  },
+
+  {
+    name: "Gatugi Girls",
+    subcounty: "Nyeri South",
+    county: "Nyeri",
+    category: "C2",
+    gender: "Girls",
+    accommodation: "Boarding",
+
+    pathways: [
+      "STEM",
+      "Social Sciences",
+      "Arts & Sports Science",
+    ],
+
+    tracks: [
+      "Pure Sciences",
+      "Applied Sciences",
+      "Technical Studies",
+      "Humanities & Business Studies",
+      "Languages & Literature",
+      "Arts",
+      "Sports & Recreation",
+    ],
+
+    subjectCombinations: [
+      "Advanced Mathematics, Biology, Chemistry + 1 elective",
+      "Agriculture, Computer Studies + 1 elective",
+      "Aviation, Business Studies + 1 elective",
+      "Business Studies, History & Citizenship + 1 elective",
+      "Arabic, French + 1 elective",
+      "Fine Arts, Theatre & Film + 1 elective",
+    ],
+
+    disabilitySupport: false,
+  },
+
+  {
+    name: "Karima Boys HS Boarding",
+    subcounty: "Nyeri South",
+    county: "Nyeri",
+    category: "C2",
+    gender: "Boys",
+    accommodation: "Boarding",
+
+    pathways: [
+      "STEM",
+      "Social Sciences",
+      "Arts & Sports Science",
+    ],
+
+    tracks: [
+      "Pure Sciences",
+      "Applied Sciences",
+      "Technical Studies",
+      "Humanities & Business Studies",
+      "Languages & Literature",
+      "Arts",
+      "Sports & Recreation",
+    ],
+
+    subjectCombinations: [
+      "Advanced Mathematics, Biology, Chemistry + 1 elective",
+      "Agriculture, Computer Studies + 1 elective",
+      "Aviation, Business Studies + 1 elective",
+      "Business Studies, History & Citizenship + 1 elective",
+      "Arabic, French + 1 elective",
+      "Fine Arts, Theatre & Film + 1 elective",
+    ],
+
+    disabilitySupport: false,
+  },
+
+  {
+    name: "Kenyatta H.S Mahiga",
+    subcounty: "Nyeri South",
+    county: "Nyeri",
+    category: "C2",
+    gender: "Boys",
+    accommodation: "Boarding",
+
+    pathways: [
+      "STEM",
+      "Social Sciences",
+      "Arts & Sports Science",
+    ],
+
+    tracks: [
+      "Pure Sciences",
+      "Applied Sciences",
+      "Technical Studies",
+      "Humanities & Business Studies",
+      "Languages & Literature",
+      "Arts",
+      "Sports & Recreation",
+    ],
+
+    subjectCombinations: [
+      "Advanced Mathematics, Biology, Chemistry + 1 elective",
+      "Agriculture, Computer Studies + 1 elective",
+      "Aviation, Business Studies + 1 elective",
+      "Business Studies, History & Citizenship + 1 elective",
+      "Arabic, French + 1 elective",
+      "Fine Arts, Theatre & Film + 1 elective",
+    ],
+
+    disabilitySupport: false,
+  },
+
+  {
+    name: "O.L.O.F Chinga Girls",
+    subcounty: "Nyeri South",
+    county: "Nyeri",
+    category: "C2",
+    gender: "Girls",
+    accommodation: "Boarding",
+
+    pathways: [
+      "STEM",
+      "Social Sciences",
+      "Arts & Sports Science",
+    ],
+
+    tracks: [
+      "Pure Sciences",
+      "Applied Sciences",
+      "Technical Studies",
+      "Humanities & Business Studies",
+      "Languages & Literature",
+      "Arts",
+      "Sports & Recreation",
+    ],
+
+    subjectCombinations: [
+      "Advanced Mathematics, Biology, Chemistry + 1 elective",
+      "Agriculture, Computer Studies + 1 elective",
+      "Aviation, Business Studies + 1 elective",
+      "Business Studies, History & Citizenship + 1 elective",
+      "Arabic, French + 1 elective",
+      "Fine Arts, Theatre & Film + 1 elective",
+    ],
+
+    disabilitySupport: false,
+  },
+
+  {
+    name: "Othaya Girls",
+    subcounty: "Nyeri South",
+    county: "Nyeri",
+    category: "C2",
+    gender: "Girls",
+    accommodation: "Boarding",
+
+    pathways: [
+      "STEM",
+      "Social Sciences",
+      "Arts & Sports Science",
+    ],
+
+    tracks: [
+      "Pure Sciences",
+      "Applied Sciences",
+      "Technical Studies",
+      "Humanities & Business Studies",
+      "Languages & Literature",
+      "Arts",
+      "Sports & Recreation",
+    ],
+
+    subjectCombinations: [
+      "Advanced Mathematics, Biology, Chemistry + 1 elective",
+      "Agriculture, Computer Studies + 1 elective",
+      "Aviation, Business Studies + 1 elective",
+      "Business Studies, History & Citizenship + 1 elective",
+      "Arabic, French + 1 elective",
+      "Fine Arts, Theatre & Film + 1 elective",
+    ],
+
+    disabilitySupport: false,
+  },
+
+  // C3 — COUNTY
+
+  {
+    name: "Iriaini Girls",
+    subcounty: "Nyeri South",
+    county: "Nyeri",
+    category: "C3",
+    gender: "Girls",
+    accommodation: "Boarding",
+
+    pathways: [
+      "STEM",
+      "Social Sciences",
+    ],
+
+    tracks: [
+      "Pure Sciences",
+      "Applied Sciences",
+      "Technical Studies",
+      "Humanities & Business Studies",
+      "Languages & Literature",
+    ],
+
+    subjectCombinations: [
+      "Advanced Mathematics, Biology, Chemistry + 1 elective",
+      "Agriculture, Computer Studies + 1 elective",
+      "Aviation, Business Studies + 1 elective",
+      "Business Studies, History & Citizenship + 1 elective",
+      "Arabic, French + 1 elective",
+    ],
+
+    disabilitySupport: false,
+  },
+
+  {
+    name: "Mucharage Secondary School",
+    subcounty: "Nyeri South",
+    county: "Nyeri",
+    category: "C3",
+    gender: "Girls",
+    accommodation: "Boarding",
+
+    pathways: [
+      "STEM",
+      "Social Sciences",
+    ],
+
+    tracks: [
+      "Pure Sciences",
+      "Applied Sciences",
+      "Technical Studies",
+      "Humanities & Business Studies",
+      "Languages & Literature",
+    ],
+
+    subjectCombinations: [
+      "Advanced Mathematics, Biology, Chemistry + 1 elective",
+      "Agriculture, Computer Studies + 1 elective",
+      "Aviation, Business Studies + 1 elective",
+      "Business Studies, History & Citizenship + 1 elective",
+      "Arabic, French + 1 elective",
+    ],
+
+    disabilitySupport: false,
+  },
+
+  // C4 — SUB COUNTY
+
+  {
+    name: "Gathera Sec",
+    subcounty: "Nyeri South",
+    county: "Nyeri",
+    category: "C4",
+    gender: "Mixed",
+    accommodation: "Day",
+    pathways: ["STEM"],
+    tracks: [
+      "Pure Sciences",
+      "Applied Sciences",
+      "Technical Studies",
+    ],
+    subjectCombinations: [
+      "Advanced Mathematics, Biology, Chemistry + 1 elective",
+      "Agriculture, Computer Studies + 1 elective",
+      "Aviation, Business Studies + 1 elective",
+    ],
+    disabilitySupport: false,
+  },
+
+  {
+    name: "Gathumbi Secondary School",
+    subcounty: "Nyeri South",
+    county: "Nyeri",
+    category: "C4",
+    gender: "Mixed",
+    accommodation: "Day",
+    pathways: ["STEM"],
+    tracks: [
+      "Pure Sciences",
+      "Applied Sciences",
+      "Technical Studies",
+    ],
+    subjectCombinations: [
+      "Advanced Mathematics, Biology, Chemistry + 1 elective",
+      "Agriculture, Computer Studies + 1 elective",
+      "Aviation, Business Studies + 1 elective",
+    ],
+    disabilitySupport: false,
+  },
+
+  {
+    name: "Gatugi Mixed Day Sec School",
+    subcounty: "Nyeri South",
+    county: "Nyeri",
+    category: "C4",
+    gender: "Mixed",
+    accommodation: "Day",
+    pathways: ["STEM"],
+    tracks: [
+      "Pure Sciences",
+      "Applied Sciences",
+      "Technical Studies",
+    ],
+    subjectCombinations: [
+      "Advanced Mathematics, Biology, Chemistry + 1 elective",
+      "Agriculture, Computer Studies + 1 elective",
+      "Aviation, Business Studies + 1 elective",
+    ],
+    disabilitySupport: false,
+  },
+
+  {
+    name: "Gitugi Sec",
+    subcounty: "Nyeri South",
+    county: "Nyeri",
+    category: "C4",
+    gender: "Girls",
+    accommodation: "Day",
+    pathways: ["STEM"],
+    tracks: [
+      "Pure Sciences",
+      "Applied Sciences",
+      "Technical Studies",
+    ],
+    subjectCombinations: [
+      "Advanced Mathematics, Biology, Chemistry + 1 elective",
+      "Agriculture, Computer Studies + 1 elective",
+      "Aviation, Business Studies + 1 elective",
+    ],
+    disabilitySupport: false,
+  },
+
+  {
+    name: "Gitundu",
+    subcounty: "Nyeri South",
+    county: "Nyeri",
+    category: "C4",
+    gender: "Mixed",
+    accommodation: "Day",
+    pathways: ["STEM"],
+    tracks: [
+      "Pure Sciences",
+      "Applied Sciences",
+      "Technical Studies",
+    ],
+    subjectCombinations: [
+      "Advanced Mathematics, Biology, Chemistry + 1 elective",
+      "Agriculture, Computer Studies + 1 elective",
+      "Aviation, Business Studies + 1 elective",
+    ],
+    disabilitySupport: false,
+  },
+
+  {
+    name: "Ihuririo Sec",
+    subcounty: "Nyeri South",
+    county: "Nyeri",
+    category: "C4",
+    gender: "Mixed",
+    accommodation: "Day",
+    pathways: ["STEM"],
+    tracks: [
+      "Pure Sciences",
+      "Applied Sciences",
+      "Technical Studies",
+    ],
+    subjectCombinations: [
+      "Advanced Mathematics, Biology, Chemistry + 1 elective",
+      "Agriculture, Computer Studies + 1 elective",
+      "Aviation, Business Studies + 1 elective",
+    ],
+    disabilitySupport: false,
+  },
+
+  {
+    name: "Irindi Sec",
+    subcounty: "Nyeri South",
+    county: "Nyeri",
+    category: "C4",
+    gender: "Mixed",
+    accommodation: "Day",
+    pathways: ["STEM"],
+    tracks: [
+      "Pure Sciences",
+      "Applied Sciences",
+      "Technical Studies",
+    ],
+    subjectCombinations: [
+      "Advanced Mathematics, Biology, Chemistry + 1 elective",
+      "Agriculture, Computer Studies + 1 elective",
+      "Aviation, Business Studies + 1 elective",
+    ],
+    disabilitySupport: false,
+  },
+
+  {
+    name: "Kabebero Secondary School",
+    subcounty: "Nyeri South",
+    county: "Nyeri",
+    category: "C4",
+    gender: "Mixed",
+    accommodation: "Day",
+    pathways: ["STEM"],
+    tracks: [
+      "Pure Sciences",
+      "Applied Sciences",
+      "Technical Studies",
+    ],
+    subjectCombinations: [
+      "Advanced Mathematics, Biology, Chemistry + 1 elective",
+      "Agriculture, Computer Studies + 1 elective",
+      "Aviation, Business Studies + 1 elective",
+    ],
+    disabilitySupport: false,
+  },
+
+  {
+    name: "Kagonye Boys Sec Sch",
+    subcounty: "Nyeri South",
+    county: "Nyeri",
+    category: "C4",
+    gender: "Boys",
+    accommodation: "Day",
+    pathways: ["STEM"],
+    tracks: [
+      "Pure Sciences",
+      "Applied Sciences",
+      "Technical Studies",
+    ],
+    subjectCombinations: [
+      "Advanced Mathematics, Biology, Chemistry + 1 elective",
+      "Agriculture, Computer Studies + 1 elective",
+      "Aviation, Business Studies + 1 elective",
+    ],
+    disabilitySupport: false,
+  },
+
+  {
+    name: "Kagumo Day",
+    subcounty: "Nyeri South",
+    county: "Nyeri",
+    category: "C4",
+    gender: "Mixed",
+    accommodation: "Day",
+    pathways: ["STEM"],
+    tracks: [
+      "Pure Sciences",
+      "Applied Sciences",
+      "Technical Studies",
+    ],
+    subjectCombinations: [
+      "Advanced Mathematics, Biology, Chemistry + 1 elective",
+      "Agriculture, Computer Studies + 1 elective",
+      "Aviation, Business Studies + 1 elective",
+    ],
+    disabilitySupport: false,
+  },
+
+  {
+    name: "Kairuthi",
+    subcounty: "Nyeri South",
+    county: "Nyeri",
+    category: "C4",
+    gender: "Mixed",
+    accommodation: "Day",
+    pathways: ["STEM"],
+    tracks: [
+      "Pure Sciences",
+      "Applied Sciences",
+      "Technical Studies",
+    ],
+    subjectCombinations: [
+      "Advanced Mathematics, Biology, Chemistry + 1 elective",
+      "Agriculture, Computer Studies + 1 elective",
+      "Aviation, Business Studies + 1 elective",
+    ],
+    disabilitySupport: false,
+  },
+
+  {
+    name: "Kariko Sec",
+    subcounty: "Nyeri South",
+    county: "Nyeri",
+    category: "C4",
+    gender: "Mixed",
+    accommodation: "Day",
+    pathways: ["STEM"],
+    tracks: [
+      "Pure Sciences",
+      "Applied Sciences",
+      "Technical Studies",
+    ],
+    subjectCombinations: [
+      "Advanced Mathematics, Biology, Chemistry + 1 elective",
+      "Agriculture, Computer Studies + 1 elective",
+      "Aviation, Business Studies + 1 elective",
+    ],
+    disabilitySupport: false,
+  },
+
+  {
+    name: "Karima Day",
+    subcounty: "Nyeri South",
+    county: "Nyeri",
+    category: "C4",
+    gender: "Mixed",
+    accommodation: "Day",
+    pathways: ["STEM"],
+    tracks: [
+      "Pure Sciences",
+      "Applied Sciences",
+      "Technical Studies",
+    ],
+    subjectCombinations: [
+      "Advanced Mathematics, Biology, Chemistry + 1 elective",
+      "Agriculture, Computer Studies + 1 elective",
+      "Aviation, Business Studies + 1 elective",
+    ],
+    disabilitySupport: false,
+  },
+
+  {
+    name: "Kiaguthu",
+    subcounty: "Nyeri South",
+    county: "Nyeri",
+    category: "C4",
+    gender: "Mixed",
+    accommodation: "Day",
+    pathways: ["STEM"],
+    tracks: [
+      "Pure Sciences",
+      "Applied Sciences",
+      "Technical Studies",
+    ],
+    subjectCombinations: [
+      "Advanced Mathematics, Biology, Chemistry + 1 elective",
+      "Agriculture, Computer Studies + 1 elective",
+      "Aviation, Business Studies + 1 elective",
+    ],
+    disabilitySupport: false,
+  },
+
+  {
+    name: "Kiahagu Sec",
+    subcounty: "Nyeri South",
+    county: "Nyeri",
+    category: "C4",
+    gender: "Mixed",
+    accommodation: "Day",
+    pathways: ["STEM"],
+    tracks: [
+      "Pure Sciences",
+      "Applied Sciences",
+      "Technical Studies",
+    ],
+    subjectCombinations: [
+      "Advanced Mathematics, Biology, Chemistry + 1 elective",
+      "Agriculture, Computer Studies + 1 elective",
+      "Aviation, Business Studies + 1 elective",
+    ],
+    disabilitySupport: false,
+  },
+
+  {
+    name: "Kihome Sec",
+    subcounty: "Nyeri South",
+    county: "Nyeri",
+    category: "C4",
+    gender: "Mixed",
+    accommodation: "Day",
+    pathways: ["STEM"],
+    tracks: [
+      "Pure Sciences",
+      "Applied Sciences",
+      "Technical Studies",
+    ],
+    subjectCombinations: [
+      "Advanced Mathematics, Biology, Chemistry + 1 elective",
+      "Agriculture, Computer Studies + 1 elective",
+      "Aviation, Business Studies + 1 elective",
+    ],
+    disabilitySupport: false,
+  },
+
+  {
+    name: "Kihuri Secondary",
+    subcounty: "Nyeri South",
+    county: "Nyeri",
+    category: "C4",
+    gender: "Mixed",
+    accommodation: "Day",
+    pathways: ["STEM"],
+    tracks: [
+      "Pure Sciences",
+      "Applied Sciences",
+      "Technical Studies",
+    ],
+    subjectCombinations: [
+      "Advanced Mathematics, Biology, Chemistry + 1 elective",
+      "Agriculture, Computer Studies + 1 elective",
+      "Aviation, Business Studies + 1 elective",
+    ],
+    disabilitySupport: false,
+  },
+
+  {
+    name: "Mumbuini Mixed Day",
+    subcounty: "Nyeri South",
+    county: "Nyeri",
+    category: "C4",
+    gender: "Mixed",
+    accommodation: "Day",
+    pathways: ["STEM"],
+    tracks: [
+      "Pure Sciences",
+      "Applied Sciences",
+      "Technical Studies",
+    ],
+    subjectCombinations: [
+      "Advanced Mathematics, Biology, Chemistry + 1 elective",
+      "Agriculture, Computer Studies + 1 elective",
+      "Aviation, Business Studies + 1 elective",
+    ],
+    disabilitySupport: false,
+  },
+
+  {
+    name: "Munyange Sec",
+    subcounty: "Nyeri South",
+    county: "Nyeri",
+    category: "C4",
+    gender: "Mixed",
+    accommodation: "Day",
+    pathways: ["STEM"],
+    tracks: [
+      "Pure Sciences",
+      "Applied Sciences",
+      "Technical Studies",
+    ],
+    subjectCombinations: [
+      "Advanced Mathematics, Biology, Chemistry + 1 elective",
+      "Agriculture, Computer Studies + 1 elective",
+      "Aviation, Business Studies + 1 elective",
+    ],
+    disabilitySupport: false,
+  },
+
+  {
+    name: "PCEA Gakuyu Mahiga Sec",
+    subcounty: "Nyeri South",
+    county: "Nyeri",
+    category: "C4",
+    gender: "Mixed",
+    accommodation: "Day",
+    pathways: ["STEM"],
+    tracks: [
+      "Pure Sciences",
+      "Applied Sciences",
+      "Technical Studies",
+    ],
+    subjectCombinations: [
+      "Advanced Mathematics, Biology, Chemistry + 1 elective",
+      "Agriculture, Computer Studies + 1 elective",
+      "Aviation, Business Studies + 1 elective",
+    ],
+    disabilitySupport: false,
+  },
+
+  {
+    name: "St Marys Karuthi",
+    subcounty: "Nyeri South",
+    county: "Nyeri",
+    category: "C4",
+    gender: "Mixed",
+    accommodation: "Day",
+    pathways: ["STEM"],
+    tracks: [
+      "Pure Sciences",
+      "Applied Sciences",
+      "Technical Studies",
+    ],
+    subjectCombinations: [
+      "Advanced Mathematics, Biology, Chemistry + 1 elective",
+      "Agriculture, Computer Studies + 1 elective",
+      "Aviation, Business Studies + 1 elective",
+    ],
+    disabilitySupport: false,
+  },
+
+  {
+    name: "St. Maria Goretti",
+    subcounty: "Nyeri South",
+    county: "Nyeri",
+    category: "C4",
+    gender: "Mixed",
+    accommodation: "Day",
+    pathways: ["STEM"],
+    tracks: [
+      "Pure Sciences",
+      "Applied Sciences",
+      "Technical Studies",
+    ],
+    subjectCombinations: [
+      "Advanced Mathematics, Biology, Chemistry + 1 elective",
+      "Agriculture, Computer Studies + 1 elective",
+      "Aviation, Business Studies + 1 elective",
+    ],
+    disabilitySupport: false,
+  },
+
+  {
+    name: "Witima Secondary School",
+    subcounty: "Nyeri South",
+    county: "Nyeri",
+    category: "C4",
+    gender: "Mixed",
+    accommodation: "Day",
+    pathways: ["STEM"],
+    tracks: [
+      "Pure Sciences",
+      "Applied Sciences",
+      "Technical Studies",
+    ],
+    subjectCombinations: [
+      "Advanced Mathematics, Biology, Chemistry + 1 elective",
+      "Agriculture, Computer Studies + 1 elective",
+      "Aviation, Business Studies + 1 elective",
+    ],
+    disabilitySupport: false,
+  },
+
+];
+
+export default nyeriSouthSchools;
