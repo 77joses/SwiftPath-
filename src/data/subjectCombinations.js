f53859694e5153c888d3a5784e40be1439120d9b
77joses <njigely98@gmail.com>
2026-05-21 15:11:19 +0300

    Update subjectCombinations.js

--- src/data/subjectCombinations.js
+++ src/data/subjectCombinations.js
@@ -1,16 +1,61 @@
 const subjectCombinations = [
-  "Advanced Mathematics, Biology, Chemistry",
-  "Advanced Mathematics, Biology, Physics",
-  "Advanced Mathematics, Chemistry, Physics",
-  "Biology, Chemistry, Physics",
-  "Agriculture, Computer Studies",
-  "Agriculture, Home Science",
-  "Computer Studies, Geography",
-  "Business Studies, History & Citizenship",
-  "CRE, Geography",
-  "History & Citizenship, Geography",
-  "Fine Arts, Theatre & Film",
-  "Music & Dance, Theatre & Film"
+
+  // STEM — Pure Sciences
+  "Advanced Mathematics, Biology, Chemistry + 1 elective",
+  "Advanced Mathematics, Biology, Physics + 1 elective",
+  "Advanced Mathematics, Chemistry, Physics + 1 elective",
+  "Biology, Chemistry, Physics + 1 elective",
+  "Advanced Mathematics, Physics + 1 elective",
+  "Advanced Mathematics, General Science + 1 elective",
+  "Biology, Chemistry + 1 elective",
+
+  // STEM — Applied Sciences
+  "Agriculture, Computer Studies + 1 elective",
+  "Agriculture, Home Science + 1 elective",
+  "Agriculture, Geography + 1 elective",
+  "Agriculture, Business Studies + 1 elective",
+  "Computer Studies, Geography + 1 elective",
+  "Computer Studies, Home Science + 1 elective",
+  "Computer Studies, Business Studies + 1 elective",
+
+  // STEM — Technical Studies
+  "Aviation, Business Studies + 1 elective",
+  "Aviation, General Science + 1 elective",
+  "Aviation, Geography + 1 elective",
+  "Building Construction, Business Studies + 1 elective",
+  "Building Construction, Geography + 1 elective",
+  "Electricity, Business Studies + 1 elective",
+  "Metal Work, Business Studies + 1 elective",
+  "Power Mechanics, Business Studies + 1 elective",
+  "Woodwork, Business Studies + 1 elective",
+
+  // SOCIAL SCIENCES — Humanities & Business Studies
+  "Business Studies, History & Citizenship + 1 elective",
+  "CRE/IRE/HRE, Business Studies + 1 elective",
+  "CRE/IRE/HRE, Geography + 1 elective",
+  "CRE/IRE/HRE, History & Citizenship + 1 elective",
+  "Geography, Business Studies + 1 elective",
+  "History & Citizenship, Geography + 1 elective",
+
+  // SOCIAL SCIENCES — Languages & Literature
+  "Arabic, French + 1 elective",
+  "Fasihi ya Kiswahili, Sign Language + 1 elective",
+  "French, German + 1 elective",
+  "Indigenous Language, Fasihi ya Kiswahili + 1 elective",
+  "Indigenous Language, Literature in English + 1 elective",
+  "Literature in English, Fasihi ya Kiswahili + 1 elective",
+  "Mandarin, French + 1 elective",
+  "Sign Language, Arabic + 1 elective",
+
+  // ARTS & SPORTS SCIENCE — Arts
+  "Fine Arts, Theatre & Film + 1 elective",
+  "Music & Dance, Fine Arts + 1 elective",
+  "Theatre & Film, Music & Dance + 1 elective",
+
+  // ARTS & SPORTS SCIENCE — Sports & Recreation
+  "Sports & Recreation, Biology + 1 elective",
+  "Sports & Recreation, General Science + 1 elective",
+
 ];
 
 export default subjectCombinations;
