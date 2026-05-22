2b42c50d1fc84dfab0677333e675a961d6dc3ae3
77joses <njigely98@gmail.com>
2026-05-22 21:30:18 +0300

    Update performanceBands.js

--- src/data/performanceBands.js
+++ src/data/performanceBands.js
@@ -1,10 +1,12 @@
 const performanceBands = [
-  "EE1",
-  "EE2",
-  "ME1",
-  "ME2",
-  "AE",
-  "BE"
+  "EE1 (90–100%)",
+  "EE2 (75–89%)",
+  "ME1 (58–74%)",
+  "ME2 (41–57%)",
+  "AE1 (31–40%)",
+  "AE2 (21–30%)",
+  "BE1 (11–20%)",
+  "BE2 (1–10%)",
 ];
 
 export default performanceBands;
